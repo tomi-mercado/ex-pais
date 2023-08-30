8d57dd7f755e9822a42602fc53a899afb444b794
Tomas <tmercadoslp@gmail.com>
2023-08-29 23:09:10 -0300

    fix: now appears december 😅

--- api.ts
+++ api.ts
@@ -8,17 +8,20 @@ const firstDateAvailable = "2017-01-01";
 const inflationUrl = `https://apis.datos.gob.ar/series/api/series/?ids=145.3_INGNACUAL_DICI_M_38&collapse=month&start_date=${firstDateAvailable}&format=json`;
 
 const getInflationPerMonth = async () => {
-  const response = await fetch(inflationUrl);
+  const response = await fetch(inflationUrl, {
+    next: {
+      revalidate: 1,
+    },
+  });
   const { data } = (await response.json()) as Response;
 
   const inflationPerMonth = data.reduce((acc, [dateStr, value]) => {
     const date = new Date(`${dateStr}:`);
 
-    let month = date.getMonth();
-    let year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
 
-    year = month === 11 ? year + 1 : year;
-    month = month === 11 ? 1 : month + 1;
+    console.log({ dateStr, month, year });
 
     const key = generateKeyMonthYear(month, year);
 
