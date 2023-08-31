d20089d7e8073ce51c02d74155b0a26de52b388e
Tomas <tmercadoslp@gmail.com>
2023-08-31 15:47:25 -0300

    refactor: separate InflationResult

--- components/InflationCalculator.tsx
+++ components/InflationCalculator.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { addZeroIfNecessary, generateKeyMonthYear } from "@/utils";
-import Image from "next/image";
+import { generateKeyMonthYear } from "@/utils";
 import React, { useRef, useState } from "react";
+import InflationResult from "./InflationResult";
 import MonthYearSelector from "./MonthYearSelector";
 
 interface InflationCalculatorProps {
@@ -194,42 +194,14 @@ const InflationCalculator: React.FC<InflationCalculatorProps> = ({
         </button>
       </form>
 
-      {!!result ? (
-        <div className="flex flex-col gap-1 h-32 text-center items-center">
-          <p className="text-lg">
-            La inflación acumulada entre{" "}
-            <strong>
-              {addZeroIfNecessary(fromMonth)}/{fromYear}
-            </strong>{" "}
-            y{" "}
-            <strong>
-              {addZeroIfNecessary(toMonth)}/{toYear}
-            </strong>{" "}
-            fue de
-          </p>
-          <p className="text-2xl text-red-700 bg-red-100 w-fit p-3 rounded-md">
-            <strong>{result.toFixed(2)}%</strong>
-          </p>
-        </div>
-      ) : (
-        <div className="relative h-32 w-32 flex items-center justify-center">
-          {!isExploding && <p className="text-8xl">🇦🇷</p>}
-
-          <Image
-            priority
-            src="/explosion.gif"
-            alt="Explosion"
-            style={{
-              zIndex: 1,
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-              display: isExploding ? "block" : "none",
-            }}
-            layout="fill"
-          />
-        </div>
-      )}
+      <InflationResult
+        fromMonth={fromMonth}
+        fromYear={fromYear}
+        toMonth={toMonth}
+        toYear={toYear}
+        result={result}
+        isExploding={isExploding}
+      />
     </div>
   );
 };
---
+++ components/InflationResult.tsx
@@ -0,0 +1,64 @@
+import { addZeroIfNecessary } from "@/utils";
+import Image from "next/image";
+import React from "react";
+
+interface InflationResultProps {
+  result: number | null;
+  isExploding: boolean;
+  fromMonth: number;
+  fromYear: number;
+  toMonth: number;
+  toYear: number;
+}
+
+const InflationResult: React.FC<InflationResultProps> = ({
+  fromYear,
+  fromMonth,
+  isExploding,
+  result,
+  toMonth,
+  toYear,
+}) => {
+  return (
+    <>
+      {!!result ? (
+        <div className="flex flex-col gap-1 h-32 text-center items-center">
+          <p className="text-lg">
+            La inflación acumulada entre{" "}
+            <strong>
+              {addZeroIfNecessary(fromMonth)}/{fromYear}
+            </strong>{" "}
+            y{" "}
+            <strong>
+              {addZeroIfNecessary(toMonth)}/{toYear}
+            </strong>{" "}
+            fue de
+          </p>
+          <p className="text-2xl text-red-700 bg-red-100 w-fit p-3 rounded-md">
+            <strong>{result.toFixed(2)}%</strong>
+          </p>
+        </div>
+      ) : (
+        <div className="relative h-32 w-32 flex items-center justify-center">
+          {!isExploding && <p className="text-8xl">🇦🇷</p>}
+
+          <Image
+            priority
+            src="/explosion.gif"
+            alt="Explosion"
+            style={{
+              zIndex: 1,
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              display: isExploding ? "block" : "none",
+            }}
+            layout="fill"
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default InflationResult;
