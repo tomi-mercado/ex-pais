8187d8aaef0d26108c962842c0f8248ebe283a87
Tomas <tomas.mercado@aerolab.co>
2023-12-31 14:14:17 -0300

    feat: add 📉 favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Ex País",
   description: "Calcula la inflación de Argentina",
+  icons: [
+    {
+      rel: "icon",
+      url: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📉</text></svg>",
+    },
+  ],
 };
 
 export default function RootLayout({
