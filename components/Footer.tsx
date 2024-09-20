f9c4a1cbe48baecd24b97d940a8a560c8649f51a
Tomas <tmercadoslp@gmail.com>
2024-09-19 23:53:24 -0300

    feat: button to cafecito

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="px-4 py-6 w-full text-sm flex justify-center gap-5 border-t border-primary bg-muted">
-      <p className="self-center text-center">
+    <footer className="px-4 pb-6 pt-2 w-full text-sm flex flex-col justify-between items-center gap-5 border-t border-primary bg-muted">
+      <span />
+
+      <p className="text-center">
         Hecho con ❤️ desde el mejor país del mundo 🇦🇷 por{" "}
         <a
           className="font-bold"
@@ -13,6 +16,21 @@ const Footer: React.FC = () => {
           Tom
         </a>
       </p>
+
+      <a
+        href="https://cafecito.app/ex-pais"
+        rel="noopener"
+        target="_blank"
+        className="flex items-center"
+      >
+        <span className="mr-2 text-xs">¿Te gusta Ex País?</span>
+        <Image
+          src="https://cdn.cafecito.app/imgs/buttons/button_3.png"
+          alt="Invitame un café en cafecito.app"
+          width={120}
+          height={40}
+        />
+      </a>
     </footer>
   );
 };
